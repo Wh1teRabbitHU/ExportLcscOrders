0b74a922f06eb1cbe5b6411a85d8461d71f9481f
Tamas Ruszka <ruszka.tamas@gmail.com>
2022-01-16 19:11:02 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ExportLcscOrders
-Because we don't have availably any open API endpoints to LCSC, this codebase is focusing on a browser based approach. To run the code, you have to open your browser on the page where your items are and run the script, which will generate a CSV based output into your console
+
+- [ExportLcscOrders](#exportlcscorders)
+	- [Description](#description)
+	- [How to use?](#how-to-use)
+
+## Description
+
+Because we don't have availably any open API endpoints to LCSC, this codebase is focusing on a browser based approach.
+
+## How to use?
+
+ To run the code, (it's in the export.js file) you have to open your browser on the page where your items are and run the script, which will generate a CSV based output into your console.
---
+++ export.js
@@ -0,0 +1,40 @@
+function crawlPage() {
+    const CSVDelimiter = ';';
+    const rootElement = document.querySelector('.orderDetailShopList');
+    const itemContainers = rootElement.querySelectorAll('.detailShopItem > .itemContent');
+
+    const items = [];
+
+    for (let itemContainer of itemContainers) {
+        const leftContentSelector = '.itemContentL > div:nth-child(2)';
+        const centerContentSelector = '.itemContentC > div:nth-child(1) > div:nth-child(1) > div';
+        const link = "https://lcsc.com" + itemContainer.querySelector(`${leftContentSelector} > div:nth-child(1) a`).getAttribute('href');
+        const name = itemContainer.querySelector(`${leftContentSelector} > div:nth-child(2)`).innerText.replace('Mfr.#: ', '');
+        const manufacturer = itemContainer.querySelector(`${leftContentSelector} > div:nth-child(3)`).innerText.replace('Mfr: ', '');
+        const description = itemContainer.querySelector(`${leftContentSelector} > div:nth-child(4)`).innerText;
+        const qty = parseInt(itemContainer.querySelector(centerContentSelector).innerText.replace('Order Qty.: ', '').trim(), 10);
+
+
+        items.push({
+            manufacturer, name, description, qty, link
+        });
+    }
+
+    if (items.length === 0) {
+        console.log('No items found on the page!');
+
+        return;
+    }
+
+    let output = '';
+
+    output += Object.keys(items[0]).reduce((a, b) => a + CSVDelimiter + b) + '\n';
+
+    items.map(item => {
+        output += Object.values(item).reduce((a, b) => a + CSVDelimiter + b) + '\n';
+    });
+
+    return output;
+}
+
+console.log(crawlPage());
